a990fb11dfebca9c2e08659b4f1be0933a47e5c7
sydripz <sydneycdavis@gmail.com>
2021-10-27 12:56:11 -0400

    Adjust media queries

--- java/scripts.js
+++ java/scripts.js
@@ -1,9 +1,3 @@
-<script
-  src="https://code.jquery.com/jquery-3.6.0.js"
-  integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-  crossorigin="anonymous"></script>
-
-console.log("Welcome to my Portfolio Website! Stay & look a while!")
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
@@ -13,15 +7,3 @@ function menuToggle() {
     x.className = 'navtoggle';
   }
 }
-
-nav > ul > li {
-  display: inline-block;
-  min-width: 641px;
-}
-
-.navtoggle li:not(:last-child) {
-  display: inline-block;}
-
-  .navtoggle li:last-child {
-    display: none;
-  }
